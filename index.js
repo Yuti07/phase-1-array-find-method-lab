9d927df88b01c9f8b07946f2b38ed696b1b936f7
Yuti Thakkar <thakkaryuti@gmail.com>
2023-04-22 13:43:37 -0400

    adds array method

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+  ]
+console.log(superbowlWin(games));
+function superbowlWin(record) {
+    const winRecord = record.find((game) => game.result === "W");
+    return winRecord ? winRecord.year : undefined;
+  }
+
+
